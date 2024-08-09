35ba535705c517d615a2a3b60a2fff79aa90a7b4
Trust <90010323+TrustCoodes@users.noreply.github.com>
2024-08-09 22:59:59 +0100

    Commmit txn

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x7dbba743b2343a863afb1ee9f3217eec3b2a4b8a84300d27ded8da7f582ceff4"],
+      accounts: ["0xcEa14331e97AD289397d5273C24Cce0062126592"],
     },
   },
 };
